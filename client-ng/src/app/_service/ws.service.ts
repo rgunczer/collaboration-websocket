8a9415fdd6f2cf11aec13c82b6b78b23b81c0f06
rgunczer <rgunczer@gmail.com>
2023-02-14 03:07:14 +0100

    refactor and trace the collaboration flow

--- client-ng/src/app/_service/ws.service.ts
+++ client-ng/src/app/_service/ws.service.ts
@@ -99,7 +99,7 @@ export class WsService {
     const collaborator = JSON.parse(message.body);
     this.preProcessCollaborator(collaborator);
     this.collaborators.push(collaborator);
-    this.sortCollaboratorsByTime();
+    this.sortCollaboratorsByTime(this.collaborators);
   }
 
   logoutCallback = (message: Stomp.Frame) => {
@@ -109,7 +109,7 @@ export class WsService {
     const index = this.collaborators.findIndex(x => x.sessionId === collaborator.sessionId);
     if (index !== -1) {
       this.collaborators.splice(index, 1);
-      this.sortCollaboratorsByTime();
+      this.sortCollaboratorsByTime(this.collaborators);
     }
   }
 
@@ -120,16 +120,99 @@ export class WsService {
     collaborators.forEach(c => this.preProcessCollaborator(c));
 
     this.collaborators = collaborators;
-    this.sortCollaboratorsByTime();
+    this.sortCollaboratorsByTime(this.collaborators);
   }
 
   getOnEvents$(): Observable<any> {
     return this.events.asObservable();
   }
 
-  sendMessage(path: string, obj: any): void {
+  private sendMessage(path: string, obj: any): void {
     this.client?.send(path, {}, JSON.stringify(obj));
   }
+
+  sendMouseMovement(entityId: string, event: MouseEvent): void {
+    this.sendMessage(
+      `/app/editing/${entityId}/mouse-moving`,
+      {
+        name: this.nickName,
+        mx: event.clientX, // TODO: calc proper pos based on scroll etc.
+        my: event.clientY
+      }
+    );
+  }
+
+  sendFieldInput(entityId: string, fieldName: string, fieldValue: any): void {
+    this.sendMessage(
+      `/app/editing/${entityId}/field`,
+      {
+        type: 'input',
+        nick: this.nickName,
+        field: fieldName,
+        value: fieldValue,
+      }
+    );
+  }
+
+  sendFieldFocus(entityId: string, fieldName: string): void {
+    this.sendMessage(
+      `/app/editing/${entityId}/field`,
+      {
+        type: 'focus',
+        nick: this.nickName,
+        field: fieldName,
+        value: '',
+      }
+    );
+  }
+
+  sendFieldBlur(entityId: string, fieldName: string): void {
+    this.sendMessage(
+      `/app/editing/${entityId}/field`,
+      {
+        type: 'blur',
+        nick: this.nickName,
+        field: fieldName,
+        value: '',
+      }
+    );
+  }
+
+  getCollaboratorsAndCurrentState(
+    entityId: string,
+    collaboratorsOnThisEntity: Collaborator[],
+    currentEntityStatus: any
+  ): Promise<any> {
+    collaboratorsOnThisEntity.length = 0;
+    return new Promise(
+      (resolve, reject) => {
+
+        const sub = this.client?.subscribe(
+          `/app/begin/${entityId}`,
+          (frame: Stomp.Frame) => {
+            const msg = JSON.parse(frame.body);
+
+            debugger;
+
+            this.collaborators.forEach(c => {
+              if (msg.collaboratorNames.includes(c.nick)) {
+                collaboratorsOnThisEntity.push(c);
+              }
+            });
+
+            sub?.unsubscribe();
+
+            resolve({ msg });
+          },
+          {
+            snapshot: JSON.stringify(currentEntityStatus)
+          }
+        );
+
+      }
+    );
+  }
+
 
   private preProcessCollaborator(c: CollaboratorEx): void {
     c.bgColor = c.color;
@@ -137,8 +220,8 @@ export class WsService {
     c.time = new Date(c.time);
   }
 
-  private sortCollaboratorsByTime(): void {
-    this.collaborators.sort((a, b) => a.time.getTime() - b.time.getTime());
+  private sortCollaboratorsByTime(collaborators: Collaborator[]): void {
+    collaborators.sort((a, b) => a.time.getTime() - b.time.getTime());
   }
 
 }
--- client-ng/src/app/person/edit/person-edit.component.ts
+++ client-ng/src/app/person/edit/person-edit.component.ts
@@ -20,17 +20,15 @@ export class PersonEditComponent implements OnInit {
   id!: string | null;
   name = "";
   age = 0;
-  nick = "-";
+
+  get nick(): string {
+    return this.ws.nickName;
+  }
 
   nameOwner: Collaborator | null = null;
-  nameStatus = "-";
-
   ageOwner: Collaborator | null = null;
-  ageStatus = "-";
-
 
   collaborators: Collaborator[] = [];
-
 
 
   constructor(
@@ -42,130 +40,142 @@ export class PersonEditComponent implements OnInit {
   }
 
   ngOnInit(): void {
-      this.nick = this.ws.nickName;
+    console.log('ngOnInit');
 
-      this.id = this.route.snapshot.paramMap.get('id');
-      if (this.id) {
-        const person = this.svc.getPersonById(parseInt(this.id));
-
-        if (person) {
-          this.name = person.name;
-          this.age = person.age;
-
-          this.entityId = 'edit.' + this.id;
-
-          fromEvent<MouseEvent>(document, 'mousemove')
-            .pipe(
-              filter(() => this.ws.isConnected),
-              sampleTime(100),
-              takeUntil(this.destroy$)
-            )
-            .subscribe((e: MouseEvent) => {
-              const path = `/app/editing/${this.entityId}/mouse-moving`;
-              this.ws.sendMessage(
-                path,
-                {
-                  name: this.ws.nickName,
-                  mx: e.clientX,
-                  my: e.clientY
-                }
-              );
-            });
-
-
-          if (this.ws.isConnected) {
-            this.ws.sendMessage(`/app/editing/${this.entityId}/enter`, {
-              type: 'person',
-              entity_id: this.id,
-              nick: this.ws.nickName,
-              fields: [
-                {
-                  name: 'name',
-                  value: this.name,
-                  owner: '-'
-                },
-                {
-                  name: 'age',
-                  value: this.age,
-                  owner: '-'
-                }
-              ]
-            });
-
-            this.ws.client?.subscribe(`/topic/editing/${this.entityId}/enter`, (message:any) => {
-              if (message.body) {
-                const msg = JSON.parse(message.body);
-
-                // debugger;
-                this.collaborators = [];
-                this.ws.collaborators.forEach(c => {
-                  if (msg.collaboratorNames.includes(c.nick)) {
-                    this.collaborators.push(c);
-                  }
-                });
-
-              }
-            });
-
-            this.ws.client?.subscribe(`/topic/editing/${this.entityId}/mouse-moving`, (message:any) => {
-              if (message.body) {
-                const msg = JSON.parse(message.body);
-
-                for(let i = 0; i < this.ws.collaborators.length; ++i) {
-                  const collaborator = this.ws.collaborators[i];
-                  if (collaborator.nick === msg.name) {
-                    collaborator.mouseX = msg.mx;
-                    collaborator.mouseY = msg.my;
-
-                    break;
-                  }
-                }
-
-              }
-            });
-
-            this.ws.client?.subscribe(`/topic/editing/${this.entityId}/field`, (message: any) => {
-              if (message.body) {
-                const msg: FieldAction = JSON.parse(message.body);
-
-                for (let i = 0; i < this.ws.collaborators.length; ++i) {
-                  const collaborator = this.ws.collaborators[i];
-                  if (collaborator.nick === msg.nick) {
-                    if (msg.field === 'name') {
-                      if (msg.type === 'focus') {
-                        this.nameOwner = collaborator;
-                      }
-                      if (msg.type === 'blur' && this.nameOwner === collaborator) {
-                        this.nameOwner = null;
-                      }
-                      if (msg.type === 'input' && this.nameOwner === collaborator) {
-                        this.name = msg.value;
-                      }
-                    }
-
-                    if (msg.field === 'age') {
-                      if (msg.type === 'focus') {
-                        this.ageOwner = collaborator;
-                      }
-                      if (msg.type === 'blur' && this.ageOwner === collaborator) {
-                        this.ageOwner = null;
-                      }
-                      if (msg.type === 'input' && this.ageOwner === collaborator) {
-                        this.age = parseInt(msg.value);
-                      }
-                    }
-
-
-                  }
-                }
-
-              }
-            });
-
-          }
-
-        }
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      const id = parseInt(this.id)
+      this.load(id);
     }
+  }
+
+  private load(id: number): void {
+    console.log('load [' + id + ']');
+
+    const person = this.svc.getPersonById(id);
+    if (person) {
+      this.name = person.name;
+      this.age = person.age;
+
+      this.entityId = 'edit|' + id;
+
+      setTimeout(() => this.beginCollaboration());
+    } else {
+      console.warn('Person with [id: ${id}] NOT found!');
+    }
+  }
+
+  private beginCollaboration(): void {
+    console.log('beginCollaboration');
+
+    if (!this.ws.isConnected) {
+      console.log('not connected collaboration aborted');
+      return;
+    }
+
+    const currentEntityStatus = {
+      type: 'person',
+      entity_id: this.id,
+      nick: this.ws.nickName,
+      fields: [
+        {
+          name: 'name',
+          value: this.name,
+          owner: '-'
+        },
+        {
+          name: 'age',
+          value: this.age,
+          owner: '-'
+        }
+      ]
+    };
+
+    this.ws.getCollaboratorsAndCurrentState(this.entityId, this.collaborators, currentEntityStatus)
+      .then((msg: any) => {
+        debugger;
+        this.listenToCollaboratorMouseMoves();
+        this.listenToCollaboratorFieldActions();
+        this.sendMouseMovements();
+      });
+  }
+
+  private listenToCollaboratorMouseMoves(): void {
+    console.log('listenToCollaboratorMouseMoves');
+
+    // this.ws.client?.subscribe(`/topic/editing/${this.entityId}/mouse-moving`, (message:any) => {
+    //   if (message.body) {
+    //     const msg = JSON.parse(message.body);
+
+    //     for(let i = 0; i < this.ws.collaborators.length; ++i) {
+    //       const collaborator = this.ws.collaborators[i];
+    //       if (collaborator.nick === msg.name) {
+    //         collaborator.mouseX = msg.mx;
+    //         collaborator.mouseY = msg.my;
+
+    //         break;
+    //       }
+    //     }
+
+    //   }
+    // });
+  }
+
+  private listenToCollaboratorFieldActions(): void {
+    console.log('listenToCollaboratorFieldActions');
+
+    // this.ws.client?.subscribe(`/topic/editing/${this.entityId}/field`, (message: any) => {
+    //   if (message.body) {
+    //     const msg: FieldAction = JSON.parse(message.body);
+
+    //     for (let i = 0; i < this.ws.collaborators.length; ++i) {
+    //       const collaborator = this.ws.collaborators[i];
+    //       if (collaborator.nick === msg.nick) {
+    //         if (msg.field === 'name') {
+    //           if (msg.type === 'focus') {
+    //             this.nameOwner = collaborator;
+    //           }
+    //           if (msg.type === 'blur' && this.nameOwner === collaborator) {
+    //             this.nameOwner = null;
+    //           }
+    //           if (msg.type === 'input' && this.nameOwner === collaborator) {
+    //             this.name = msg.value;
+    //           }
+    //         }
+
+    //         if (msg.field === 'age') {
+    //           if (msg.type === 'focus') {
+    //             this.ageOwner = collaborator;
+    //           }
+    //           if (msg.type === 'blur' && this.ageOwner === collaborator) {
+    //             this.ageOwner = null;
+    //           }
+    //           if (msg.type === 'input' && this.ageOwner === collaborator) {
+    //             this.age = parseInt(msg.value);
+    //           }
+    //         }
+
+
+    //       }
+    //     }
+
+    //   }
+    // });
+
+  }
+
+  private sendMouseMovements(): void {
+    console.log('sendMouseMovements');
+
+    fromEvent<MouseEvent>(document, 'mousemove')
+      .pipe(
+        filter(() => this.ws.isConnected),
+        sampleTime(100),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(
+        (e: MouseEvent) => this.ws.sendMouseMovement(this.entityId, e)
+      );
   }
 
   ngOnDestroy(): void {
@@ -173,44 +183,25 @@ export class PersonEditComponent implements OnInit {
     this.destroy$.complete();
   }
 
-  onFocus(event): void {
-    const elementName = event.target.name;
-    console.log(`focus [${elementName}]`);
-
-    this.ws.sendMessage(`/app/editing/${this.entityId}/field`, {
-      type: 'focus',
-      nick: this.nick,
-      field: elementName,
-      value: '',
-    });
+  onFocus(event: any): void {
+    const fieldName = event.target.name;
+    this.ws.sendFieldFocus(this.entityId, fieldName);
   }
 
   onBlur(event): void {
-    const elementName = event.target.name;
-
-    console.log(`blur [${elementName}]`);
-
-    this.ws.sendMessage(`/app/editing/${this.entityId}/field`, {
-      type: 'blur',
-      nick: this.nick,
-      field: elementName,
-      value: '',
-    });
+    const fieldName = event.target.name;
+    this.ws.sendFieldBlur(this.entityId, fieldName);
   }
 
-  onInput(event): void {
+  onInput(event: any): void {
     const elementName = event.target.name;
     const text = event.target.value;
 
-    console.log(`input [${elementName}]`);
-
-    this.ws.sendMessage(`/app/editing/${this.entityId}/field`, {
-      type: 'input',
-      nick: this.nick,
-      field: elementName,
-      value: text,
-    });
-
+    this.ws.sendFieldInput(
+      this.entityId,
+      elementName,
+      text,
+    );
   }
 
 }
